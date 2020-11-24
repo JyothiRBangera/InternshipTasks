e386d298136722e5e250dd5ffe8b280af5c2494c
JyothiRBangera <74918401+JyothiRBangera@users.noreply.github.com>
2020-11-24 11:07:54 +0530

    Add files via upload

---
+++ Internshipexe1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+days="thirty days"
+print(days[0])
+
+
+# In[2]:
+
+
+challenge= "I will win"
+print(challenge[7:10])
+
+
+# In[3]:
+
+
+challenge= "I will"
+print(len(challenge))
+
+
+# In[4]:
+
+
+challenge= "I will win"
+print(challenge.lower())
+
+
+# In[5]:
+
+
+a="30 Days"
+b="30 hours"
+c=a+b
+print(c)
+
+
+# In[6]:
+
+
+a="30 Days"
+b="30 hours"
+c=a+" "+b
+print(c)
+
+
+# In[7]:
+
+
+text="Thirty days and Thirty hours"
+x=text.casefold()
+print(x)
+
+
+# In[8]:
+
+
+#to create a string and slicing from 2nd value to last
+#task1
+z="Python internship"
+print(z[1:])
+
+
+# In[ ]:
+
+
+
+
