0a4b975237c5b1b564707c707e2978323af46a9b
JyothiRBangera <74918401+JyothiRBangera@users.noreply.github.com>
2020-11-24 16:33:54 +0530

    Add files via upload

---
+++ _Internship task2.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[57]:
+
+
+#integers having same value to variables and performing arithmetic operations
+a=b=c=3
+x=a/10
+y=b*50
+z=c+60
+
+
+# In[58]:
+
+
+print(x,y,z)
+
+
+# In[59]:
+
+
+#replacing 3rd character with g using replace() function
+str="urban"
+print(str.replace("b","g"))
+
+
+# In[60]:
+
+
+#replacing 3rd character with g  without using replace() function
+str="urban"
+s=str[:2]+"g"+str[3:]
+s
+
+
+# In[61]:
+
+
+#converting integer to float and float to integer data type
+a,b=6.8,5
+print(type(a))
+print(type(b))
+x=int(a)
+print(type(x))
+y=float(b)
+print(type(y))
+print(x,y)
+
+
+# In[ ]:
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
