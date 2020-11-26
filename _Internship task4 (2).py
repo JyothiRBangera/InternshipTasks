d2364ba288cd0242a8b66c33d3e23e974bbdf5ff
JyothiRBangera <74918401+JyothiRBangera@users.noreply.github.com>
2020-11-26 16:14:57 +0530

    Add files via upload

---
+++ _Internship task4 (2).py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[33]:
+
+
+#to create a list and add item into the list
+num=[100,200,300,500,800]
+y=input("add the item into the list")
+num.append(y)
+print(num)
+
+
+# In[34]:
+
+
+#to delete item from the list
+num.pop()
+print(num)
+
+
+# In[35]:
+
+
+#to store the largest and smallest number from the list to a variable
+a=max(num)
+b=min(num)
+print(a,b)
+
+
+# In[36]:
+
+
+#to create a tuple and print the reverse of created tuple
+my_tuple=('may',78,90.89)
+print(my_tuple[::-1])
+
+
+# In[37]:
+
+
+#create tuple and convert it into list
+t=("car",8,90.0)
+print(type(t))
+l=list(t)
+print(type(l))
+print(l)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
