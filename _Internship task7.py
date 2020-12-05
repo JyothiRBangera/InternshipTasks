f0aa547481079c954052f1413476b76f047e2c56
JyothiRBangera <74918401+JyothiRBangera@users.noreply.github.com>
2020-12-06 00:35:49 +0530

    Add files via upload

---
+++ _Internship task7.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#create python module with list and import the module in another .py file and change value in list
+import module
+module.list1=[10,25,30,45,50,65,70,95,100]
+print(module.list1)
+
+
+# In[1]:
+
+
+#installing pandas package
+import pandas as pd
+
+
+# In[3]:
+
+
+#import a module that picks a random number
+import random
+num1=random.randint(1,100)
+print(num1)
+
+
+# In[1]:
+
+
+#import sys package and find python path
+import sys
+
+
+# In[2]:
+
+
+sys.path
+
